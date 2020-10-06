217074adda258cdb659ada683024ca93ecb46b6e
Mike Schwörer <pubgit@mikescher.com>
2020-10-06 23:46:09 +0200

    Fix WakeOnLAN under Windows10 (255.255.255.255 broadcast does not send to all adapters)
    -> https://stackoverflow.com/questions/53333351

--- Source/SynoCtrl/Tasks/Impl/WakeOnLANTask.cs
+++ Source/SynoCtrl/Tasks/Impl/WakeOnLANTask.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using MSHC.Math.Encryption;
+using MSHC.Util.Helper;
 
 namespace SynoCtrl.Tasks.Impl
 {
@@ -30,7 +33,7 @@ namespace SynoCtrl.Tasks.Impl
 
 			try
 			{
-				SendWOL(config.Selected.MACAddressRaw);
+				SendWOL(config.Selected.MACAddressRaw, config.Selected.IPAddressRaw);
 			}
 			catch (TaskException)
 			{
@@ -44,7 +47,7 @@ namespace SynoCtrl.Tasks.Impl
 			return WriteInfoOutput("Done.", $"WOL Package sent to {config.Selected.MACAddress}");
 		}
 
-		private void SendWOL(byte[] macaddr)
+		private void SendWOL(byte[] macaddr, IPAddress ipaddr)
 		{
 			var packet = new List<byte>(); 
 			for (var i = 0; i < 6; i++) packet.Add(0xFF);
@@ -59,6 +62,46 @@ namespace SynoCtrl.Tasks.Impl
 				client.Send(packet.ToArray(), packet.Count); 
 				client.Close();
 			}
+
+			if (ipaddr != null && ipaddr.AddressFamily == AddressFamily.InterNetwork)
+			{
+				WriteDebug($"Trying to determine the broadcast adress for [{ipaddr}] in all available adapters");
+				WriteDebug();
+
+				foreach (var iface in NetworkInterface.GetAllNetworkInterfaces().Where(p => p.NetworkInterfaceType != NetworkInterfaceType.Loopback).OrderBy(p => p.OperationalStatus))
+				{
+					var anymatch = false;
+					foreach (var ipinfo in iface.GetIPProperties().UnicastAddresses.AsEnumerable().Where(p => p.Address.AddressFamily == AddressFamily.InterNetwork))
+					{
+						if (ipaddr.IsPartOfSubnet(ipinfo.Address, ipinfo.IPv4Mask))
+                        {
+							WriteDebug($"Found matching subnet [{ipinfo.Address}|{ipinfo.IPv4Mask}] in adapter \"{iface.Name}\"");
+							WriteDebug();
+
+							anymatch = true;
+
+							var broadcast = ipinfo.Address.GetBroadcastAddress(ipinfo.IPv4Mask);
+							if (broadcast == null) continue;
+
+							WriteDebug($"Sending UDP package to [{broadcast}]:{42} with data\n{EncodingConverter.ByteArrayToHexDump(packet, ":", 6, "", true)}");
+							WriteDebug();
+
+							using (var client = new UdpClient())
+							{
+								client.Connect(broadcast, 42);
+								client.Send(packet.ToArray(), packet.Count);
+								client.Close();
+							}
+						}
+					}
+					if (! anymatch)
+					{
+						WriteDebug($"Found no matching subnet in adapter \"{iface.Name}\"");
+						WriteDebug();
+					}
+				}
+			}
+
 		}
 	}
 }
